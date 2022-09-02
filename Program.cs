34906537c7cd3df5c0c651f4d3242eef083f5339
Samuel Ribeiro Batista <leidymotastz@outlook.com>
2022-09-02 20:51:40 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+    //    Inicil:
+
+    //        Console.Clear();
+
+    //        Console.Write("Digite o primeiro número :");
+    //        double num1 = double.Parse(Console.ReadLine());
+
+
+
+    //        Console.Write("Digite o segundo número :");
+    //        double num2 = double.Parse(Console.ReadLine());
+
+
+
+    //        Console.Write("Escolha a Operação (+ -  x / : )");      
+    //         Char op = char.Parse(Console.ReadLine());
+    //        double resultado = 0;
+    //        switch ( op)
+    //        {
+
+
+    //            default:
+    //                Console.WriteLine("Erro opção inválida");
+    //                break;
+    //            case '+':
+    //                resultado = num1 + num2;
+    //                Console.WriteLine("O resultado da soma é: " + resultado);
+    //                break;
+
+
+    //            case '-':
+    //                resultado = num1 - num2;
+    //                Console.WriteLine("O resultado da subtração é: " + resultado);
+    //                break;
+
+
+
+    //            case 'x':
+    //            case 'X':
+
+    //                resultado = num1 * num2;
+    //                Console.WriteLine("O resultado da multipliação e:");
+    //                break;
+
+
+    //            case '/':
+    //            case ':':
+    //                if (num2 == 0)
+    //                {
+    //                    Console.WriteLine("Não é´possivel dividir por 0.");
+
+    //                }
+    //                else
+    //                {
+    //                    resultado = num1 / num2;
+    //                    Console.WriteLine("O resultado da divisão  e:");
+
+    //                }
+
+    //                break;
+
+    //                resultado = num1 / num2;
+    //                Console.WriteLine("O resultado da divisão  e:");
+    //                break;
+
+    //        }
+
+
+    //        Console.WriteLine("Continuar calculando (s / n)?");
+    //        string opcao = Console.ReadLine();
+
+    //        if (opcao == "s" || opcao == "S")
+    //        {
+    //            goto Inicil;
+    //        }
+
+
+            
+    //    }                                              
+             
+                    
+
+
+
+    //}
+
+
+}
